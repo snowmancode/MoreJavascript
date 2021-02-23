2c74ea3a7ebcb613374b3c429fa7a74b450c69a6
ShowCaseCode <showcasecode@outlook.com>
2021-02-22 22:08:37 -0500

    1st Commit on a new branch

--- MoreJSTesting.js
+++ MoreJSTesting.js
@@ -3,3 +3,7 @@ let favorite_wrestler = "Drew McIntyre";
 console.log(favorite_wrestler);
 console.log("Oh, Hello Planet Earth");
 console.log("Good Bye, Planet Earth");
+
+let best_wrestler = "Bret Hart";
+
+console.log(best_wrestler);
